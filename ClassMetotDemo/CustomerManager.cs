39e21683066e2fc2747ce8a8ad92a75a35711b9c
tahaga61 <tahaga61@gmail.com>
2021-01-26 01:31:45 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Eklendi");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Silindi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Güncellendi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoşgeldiz");
+
+            Customer customer1 = new Customer();
+            customer1.Id = 01;
+            customer1.FirstName = "Ali";
+            customer1.LastName = "Demirog";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 02;
+            customer2.FirstName = "Ayşe";
+            customer2.LastName = "Çelik";
+
+           
+            
+
+            Customer[] customers = new Customer[] { customer1,customer2};
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Id + "---" + customer.FirstName + "---" + customer.LastName + "---");
+                Console.WriteLine("********************");
+            }
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Delete();
+            customerManager.Update();
+
+
+        }
+    }
+}
